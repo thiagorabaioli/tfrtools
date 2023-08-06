deccff0e96bda06b137bdc0003510f68094b98d4
thiagorabaioli <tfrabaioli@outlook.com>
2023-08-06 17:22:37 +0100

    First commit

---
+++ src/main/java/tfr/Tools/TfrToolsApplication.java
@@ -0,0 +1,13 @@
+package tfr.Tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfrToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfrToolsApplication.class, args);
+	}
+
+}
